7b1e8a0a0007a1348ed1ef1d52f3780e19cda8bb
slrnd <wlfhrtd@gmail.com>
2022-08-31 21:57:43 +0300

    add domain entities

---
+++ Domain/Models/Department.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    [Table("Departments")]
+    public partial class Department : BaseModel
+    {
+        public Department()
+        {
+            Status = DepartmentStatus.Active;
+        }
+
+        public Department(string name) : this()
+        {
+            Name = name;
+        }
+
+        public Department(string name, DepartmentStatus status) : this(name)
+        {
+            Status = status;
+        }
+
+
+        [Required]
+        [StringLength(50)]
+        public virtual string Name { get; set; }
+
+        [NotMapped]
+        public virtual DepartmentStatus Status { get; set; }
+        // ManyToMany inside single table using DepartmentMapping
+        public virtual ICollection<Department> Departments { get; set; }
+        public virtual ICollection<Department> Subdepartments { get; set; }
+    }
+
+    public enum DepartmentStatus
+    {
+        Active,
+        Blocked,
+    }
+}
---
+++ Domain/Models/DepartmentMapping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class DepartmentMap
+    {
+        public int DepartmentId { get; set; }
+        public int SubdepartmentId { get; set; }
+    }
+}
